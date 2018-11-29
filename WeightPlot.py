2c80d23f5da7c8e8b1be9b8ca70dd5c9423d2910
Julien Moreau <moreau.juli1@gmail.com>
2018-11-29 17:41:02 +0000

    FirstCommit

---
+++ WeightPlot.py
@@ -0,0 +1,88 @@
+import os, sys, datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from scipy.signal import butter, filtfilt
+import matplotlib.ticker as ticker
+import matplotlib.patheffects as pe
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filtfilt(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
+
+WorkingDir = '/media/julien/eSATA1/Julien/Marine plastics/Balnakeil Bin/Weight'
+File = 'Weight_Data.txt'
+
+DATA  = np.genfromtxt(WorkingDir+os.sep+File, dtype=[('Date','datetime64[D]'),('Weight', 'float32'), ('Deviation', 'float32')], delimiter ='\t', skip_header = 1)
+
+Duration = np.diff(DATA['Date']).astype('timedelta64[h]')
+Days = DATA['Date'][:-1] + Duration/2
+Days = [x.tolist() for x in Days]
+Rate = DATA['Weight'][1:]/Duration.astype(float)*24
+
+cutoff = 1
+fs = 4
+Smooth = butter_lowpass_filtfilt(Rate, cutoff, fs)
+
+fig2 = plt.figure()
+ax = fig2.add_subplot(111)
+fig2.suptitle('Rate and cumulative weight of marine plastics recovered at Balnakeil Beach', fontsize=32)
+ax.xaxis_date()
+
+ax.xaxis.set_ticks_position('bottom')
+ax.xaxis.set_tick_params(which='major', direction = 'out', width=2, length = 20)
+ax.xaxis.set_tick_params(which='minor', direction = 'out', width=2, length = 0)
+ax.xaxis.set_minor_formatter(mdates.DateFormatter('%b %y'))
+ax.xaxis.set_minor_locator(mdates.MonthLocator(bymonthday=15))
+#ax.xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1, interval=1))
+ax.set_xlim(DATA['Date'][0].tolist(), DATA['Date'][-1].tolist())
+ax.set_xlabel('Date', fontsize = 16)
+ax.set_ylabel('Cumulative weight (kg)', fontsize = 16)
+ax.tick_params(axis='y', colors='midnightblue')
+ax.yaxis.label.set_color('midnightblue')
+ax.xaxis.set_major_formatter(ticker.NullFormatter())
+ax2 = ax.twinx()
+ax.bar(Days, DATA['Weight'].cumsum()[1:], width=Duration.astype(float)/24, yerr = DATA['Deviation'].cumsum()[1:], align="center", color= 'midnightblue', edgecolor ='aliceblue')
+ax2.plot(Days, Rate, lw= 2, c ='steelblue', path_effects=[pe.Stroke(linewidth=5, foreground='w'), pe.Normal()])
+#ax2.plot(Days, Smooth, lw= 3, ls='--', c='r')
+ax2.xaxis.set_major_formatter(ticker.NullFormatter())
+ax2.xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1, interval=1))
+
+ax2.set_ylabel('Rate (kg/day)', fontsize = 16)
+ax2.yaxis.label.set_color('steelblue')
+ax2.spines['right'].set_color('steelblue')
+ax2.tick_params(axis='y', colors='steelblue')
+#ax2.set_yscale('log')
+ax2.set_ylim(0.1,100)
+ax2.yaxis.set_major_formatter(ticker.FormatStrFormatter('%3.f'))
+ax2.annotate(
+    'Frequent beach cleans',
+    xy=(Days[4], Rate[4]), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[7], Rate[4]+5), fontsize=12, backgroundcolor ='w')
+ax2.annotate(
+    'Storm Aileen',
+    xy=(Days[14], Rate[14]), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[16], Rate[14]+10), fontsize=12, backgroundcolor ='w')
+
+ax2.annotate(
+     'Storm Caroline1',
+    xy=(Days[23], Rate[23]), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[18], Rate[23]+10), fontsize=12)
+ax2.annotate(
+    'Caroline2 (140 kg/day)',
+    xy=(Days[25], Rate[25]-50), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[26], Rate[25]-45), fontsize=12)
+ax2.annotate(
+    'Storms Dylan + Eleanor',
+    xy=(Days[28], Rate[28]), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[30], Rate[28]+10), fontsize=12, backgroundcolor ='w')
+ax2.annotate(
+    'Unamed Storm',
+    xy=(Days[36], Rate[36]), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[37], Rate[36]+10), fontsize=12, backgroundcolor ='w')
+ax2.annotate(
+    'Frequent beach cleans',
+    xy=(Days[51], Rate[51]), arrowprops=dict(arrowstyle='fancy', connectionstyle="arc3,rad=-0.2",ec="w",fc='k'), xytext=(Days[52], Rate[51]+10), fontsize=12, backgroundcolor ='w')
+
+plt.show()
---
+++ WeightPlot_BCG.py
@@ -0,0 +1,55 @@
+import os, sys, datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from scipy.signal import butter, filtfilt
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filtfilt(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
+
+
+WorkingDir = os.getcwd()
+File = 'BeachCleanGirl_Weight.txt'
+
+DATA  = np.genfromtxt(WorkingDir+os.sep+File, dtype=[('Date','datetime64[D]'),('Weight', 'float32')], delimiter ='\t', skip_header = 1)
+
+Duration = np.diff(DATA['Date']).astype('timedelta64[h]')
+Days = DATA['Date'][:-1] + Duration/2
+Days = [x.tolist() for x in Days]
+Rate = DATA['Weight'][1:]/Duration.astype(float)*24
+# Filter
+cutoff = 1                   
+fs = 20
+Smooth = butter_lowpass_filtfilt(Rate, cutoff, fs)
+
+fig2 = plt.figure()
+ax = fig2.add_subplot(111)
+fig2.suptitle('Rate and cumulative weight of marine plastics recovered at Lee-on-the-Solent', fontsize=16)
+ax.xaxis_date()
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
+ax2 = ax.twinx()
+ax.bar(Days, DATA['Weight'].cumsum()[1:], width=Duration.astype(float)/24, align="center", color= 'grey')
+ax2.plot(Days, Rate, lw= 2, c ='r')
+ax2.plot(Days, Smooth, lw= 3, ls='--', c ='r')
+ax.set_ylabel('Cumulative weight (kg)')
+ax2.set_ylabel('Rate (kg/day)')
+ax2.yaxis.label.set_color('red')
+ax2.spines['right'].set_color('red')
+ax2.tick_params(axis='y', colors='red')
+ax.set_xlabel('Date')
+fig2.autofmt_xdate()
+plt.show()
+
+
+    
+
